cbd3c6f336fbb1f7d3d3e21bf2185f437544bbd1
junior <junior.oliveira05@icloud.com>
2023-12-07 09:25:35 -0300

    resolvendo 2 desafio

--- Exercicios/descontoFuncionario.js
+++ Exercicios/descontoFuncionario.js
@@ -2,8 +2,6 @@
 // Faça um programa que calcule o valor total a ser pago por uma pessoa. O programa deverá ler o
 // valor total da compra efetuada e um código que identifique se o comprador é um cliente comum (1), funcionário (2) ou vip (3).
 
-
-
 //entrada de dados
 // = determinar se o cliente é 1- cliente, 2- funcionário e 3-vip
 // = informar o total da compra 
@@ -15,3 +13,36 @@
 // = se for cliente não haverá desconto
 // = se for funcionário terá 10% de desconto
 // = se for vip terá 5% de desconto
+
+
+//entrada de dados
+// = determinar se o cliente é 1- cliente, 2- funcionário e 3-vip
+
+let cliente = 4
+let produto = 100
+let desconto = 0;
+
+switch (cliente) {
+    case 1:
+        cliente = "Cliente"
+        console.log(`Olá ${cliente}, você não tem descontos nas compras total a pagar: R$ ${produto}`)
+    break;
+
+    case 2:
+        cliente = "funcionário"
+        desconto = (100*10)/100
+        produto = produto-desconto
+        console.log(`Olá ${cliente}, você tem 10% de descontos total da compra com ${desconto}% de desconto: R$ ${produto}`)
+    break;
+
+    case 3:
+        cliente = "Vip"
+        desconto = (100*5)/100
+        produto = produto-desconto
+        console.log(`Olá ${cliente}, você tem 5% de descontos total da compra com ${desconto}% de desconto: R$ ${produto}`)
+    break;
+
+    default:
+        console.log('Erro');
+    break;
+}
