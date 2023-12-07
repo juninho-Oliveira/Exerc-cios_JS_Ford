e94ce6a65d91a58793d1a2ebad8451077cd15f5a
junior <junior.oliveira05@icloud.com>
2023-12-07 12:30:48 -0300

    add function

--- Exercicios/jogoFutebol.js
+++ Exercicios/jogoFutebol.js
@@ -10,16 +10,21 @@
 
 //saida
 // = informar o resultado do jogo de futebol
-const nomeTime = "São Paulo"
-let time1 = 3
+const nomeTime = prompt('Digite o nome do primeiro time:')
+let time1 = prompt(`Digite quantos gols fez o ${nomeTime}`)
 
-const nomeTime2 = "corinthians"
-let time2 = 3
+const nomeTime2 = prompt('Digite o nome do segundo time:')
+let time2 = prompt(`Digite quantos gols fez o ${nomeTime2}`)
 
-if(time1 > time2) {
-    console.log(`Time ${nomeTime}, ganhou por ${time1} X ${time2} Do time ${nomeTime2}`)
-} else if (time2 > time1) {
-    console.log(`Time ${nomeTime2}, ganhou por ${time1} X ${time2} Do time ${nomeTime}`)
-} else {
-    console.log(`Os times ${nomeTime} e ${nomeTime2} empataram ${time2} X ${time1}`)
+function resultado(nomeTime, time1, nomeTime2, time2) {
+    if (time1 > time2) {
+        console.log(`Time ${nomeTime}, ganhou por ${time1} X ${time2} Do time ${nomeTime2}`)
+    } else if (time2 > time1) {
+        console.log(`Time ${nomeTime2}, ganhou por ${time2} X ${time1} Do time ${nomeTime}`)
+    } else {
+        console.log(`Os times ${nomeTime} e ${nomeTime2} empataram ${time2} X ${time1}`)
+    }
 }
+
+
+resultado(nomeTime, time1, nomeTime2, time2)
